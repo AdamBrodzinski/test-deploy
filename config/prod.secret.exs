5ef95a3149fda98c9edb2f5d988e7088fa1aaeed
Adam Brodzinski <adambrodzinski@gmail.com>
2016-02-17 21:56:19 -0500

    save secret for test

---
+++ config/prod.secret.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :baz, Baz.Endpoint,
+  secret_key_base: "iMgREmVpQMEh9aobO9MDMr3Oz2aXEF/yFp5Mvngk94Ip+MGEtqZbN1HROOxN9WCg"
