8a61a845cba4b1d6f3ff35f6edbd418edfb34a49
Adam Brodzinski <adambrodzinski@gmail.com>
2016-02-18 10:34:20 -0500

    bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Baz.Mixfile do
 
   def project do
     [app: :baz,
-     version: "1.0.0",
+     version: "1.1.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
