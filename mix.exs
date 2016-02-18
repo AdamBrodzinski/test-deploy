184e1b5be8f9b2049cf893206ef7e7eca46b652a
Adam Brodzinski <adambrodzinski@gmail.com>
2016-02-17 21:46:47 -0500

    adds exrm

--- mix.exs
+++ mix.exs
@@ -32,6 +32,8 @@ defmodule Baz.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.19"}
+   ]
   end
 end
